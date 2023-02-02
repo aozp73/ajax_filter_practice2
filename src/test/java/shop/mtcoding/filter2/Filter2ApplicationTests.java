ea7b47f90b131cbb339c884e933c3bc9d079dba4
aozp73 <aozp73@naver.com>
2023-02-02 21:26:15 +0900

    세팅 완료

---
+++ src/test/java/shop/mtcoding/filter2/Filter2ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.filter2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Filter2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
